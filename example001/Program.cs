f9c8ee8da29083cf2b945237a5c088d1d40cf894
ilemisova <ilemisova@mail.ru>
2022-06-27 13:59:04 +0300

    task3 do

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,2 +1,1 @@
-﻿// тренировка//
-Console.WriteLine("Hello, World!");
+﻿
---
+++ example001/tasks/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.//
+
+int a, b, max;
+Console.WriteLine("Inpute a first number: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inpute a second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+
+if(a > b)
+{
+Console.WriteLine("max " + a);
+}
+{
+Console.WriteLine("max " + b);
+}
+
